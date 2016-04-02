c4798a3d699b6276ad18cf454548d4d4eaf66d0e
Eugene Goldberg <eugene.goldberg117@gmail.com>
2016-04-02 15:41:58 -0500

    added BusinessEntityController and BusinessFunctionController

---
+++ Controllers/BusinessEntityController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http.Cors;
+using System.Web.OData;
+
+namespace SelfHostedWebApiDataService.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class BusinessEntityController : ODataController
+    {
+        Model1 db = new Model1();
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+
+        [EnableQuery]
+        public IQueryable<BusinessEntity> Get()
+        {
+            return db.BusinessEntities;
+        }
+    }
+}
