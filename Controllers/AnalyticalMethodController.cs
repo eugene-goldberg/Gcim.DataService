1b45cf8a9474276197cc2d9ca9f55065443090f7
Eugene Goldberg <eugene.goldberg117@gmail.com>
2016-04-06 08:14:38 -0500

    added all required controllers

---
+++ Controllers/AnalyticalMethodController.cs
@@ -0,0 +1,29 @@
+﻿using SelfHostedWebApiDataService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http.Cors;
+using System.Web.OData;
+
+namespace SelfHostedWebApiDataService.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class AnalyticalMethodController : ODataController
+    {
+        GCIMContext db = new GCIMContext();
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+
+        [EnableQuery]
+        public IQueryable<AnalyticalMethod> Get()
+        {
+            return db.AnalyticalMethods;
+        }
+    }
+}
---
+++ Controllers/BusinessGoalController.cs
@@ -0,0 +1,29 @@
+﻿using SelfHostedWebApiDataService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http.Cors;
+using System.Web.OData;
+
+namespace SelfHostedWebApiDataService.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class BusinessGoalController : ODataController
+    {
+        GCIMContext db = new GCIMContext();
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+
+        [EnableQuery]
+        public IQueryable<BusinessGoal> Get()
+        {
+            return db.BusinessGoals;
+        }
+    }
+}
